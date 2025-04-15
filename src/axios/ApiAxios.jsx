a8414e882e10e8b57426313baae84b6d11a9c96b
monikawebdev <monikapal1602@gmail.com>
2025-04-15 19:44:37 +0530

    update frontend

--- src/axios/ApiAxios.jsx
+++ src/axios/ApiAxios.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+
 const API_link = import.meta.env.VITE_API_URL || "http://localhost:5000";
 export const api = axios.create({
   baseURL: API_link,
-  withCredentials: true
+  withCredentials: true,
 });
 
  
